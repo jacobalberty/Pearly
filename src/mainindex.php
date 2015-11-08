664bd2cb122f85dfdc4c7dda7a6f171c2ef30002
Jacob Alberty <jacob.alberty@vetmanage.com>
2015-11-08 16:23:40 -0600

    removed setTimeZone() call

--- src/mainindex.php
+++ src/mainindex.php
@@ -24,6 +24,5 @@ if (!defined('CORE_PATH')) {
 include_once './vendor/autoload.php';
 
 set_error_handler("exception_error_handler", E_ALL);
-setTimeZone("America/Chicago");
 $router = new \Pearly\Core\Router();
 $router->invoke();
