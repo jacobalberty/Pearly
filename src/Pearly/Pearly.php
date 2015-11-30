3d9b9f02092afdb1c4501e2c527cfaf8fef8f983
Jacob Alberty <jacob.alberty@vetmanage.com>
2015-11-30 15:11:15 -0600

    Clean up Pearly.php
    Simplified setTimezone.
    Split the closure compiler code out of optimizejs into its own function.

--- src/Pearly/Pearly.php
+++ src/Pearly/Pearly.php
@@ -40,22 +40,20 @@ class Pearly
             $filename = readlink("/etc/localtime");
 
             $pos = strpos($filename, "zoneinfo");
+            $timezone = $default;
             if ($pos) {
                 // When it is, it's in the "/usr/share/zoneinfo/" folder
                 $timezone = substr($filename, $pos + strlen("zoneinfo/"));
-            } else {
-                // If not, bail
-                $timezone = $default;
             }
-        } else {
-            // On other systems, like Ubuntu, there's file with the Olsen time
-            // right inside it.
-            $timezone = trim(file_get_contents("/etc/timezone"));
-            if (!strlen($timezone)) {
-                $timezone = $default;
-            }
+            date_default_timezone_set($timezone);
+            return;
         }
-        date_default_timezone_set($timezone);
+        // On other systems, like Ubuntu, there's file with the Olsen time
+        // right inside it.
+        $timezone = trim(file_get_contents("/etc/timezone"));
+        if (!strlen($timezone)) {
+            $timezone = $default;
+        }
     }
 
     public static function optimizejs($files, $tmpdir = '../tmp/', $lib_dir = '../3rdparty/gcc/')
@@ -65,89 +63,96 @@ class Pearly
         $closure = $localcompile;
 
         if ($closure) {
-            $tmpdir = is_writable($tmpdir) ? $tmpdir : '/tmp/';
-            $afiles = [];
-            $mtime  = 0;
-            foreach (explode('&', $files) as $part) {
-                $part = explode('=', $part);
-                if ($key = array_shift($part)) {
-                    if (empty($part)) {
-                        $kfn = "{$key}.js";
-                        $afiles[] = $kfn;
-                        $cfmtime = filemtime($kfn);
-                        if ($cfmtime > $mtime) {
-                            $mtime = $cfmtime;
-                        }
-                    }
+            return self::closurejs($files, $tmpdir, $lib_dir);
+        }
+        $jsfiles = array();
+        foreach (explode('&', $files) as $part) {
+            $part = explode('=', $part);
+            if ($key = array_shift($part)) {
+                if (empty($part)) {
+                    $jsfiles[] = $key;
                 }
             }
-            header('Content-Type: text/javascript');
-            $hfname = hash('md5', implode($afiles), false) . '.js';
-            ob_start();
-            $cache_file = "{$tmpdir}/{$hfname}";
-            if (!is_readable($cache_file) || $mtime > filemtime($cache_file)) {
-                $c = new \tureki\PhpCc([
+        }
+
+        $jsc = '';        // code to compress
+        $err = '';       // error string
+        $jscomp = '';    // Final code
+        // fetch JavaScript files
+        for ($i = 0, $j = count($jsfiles); $i < $j; $i++) {
+            $fname = $jsfiles[$i] . '.js';
+            $jscode = @file_get_contents($fname);
+            if ($jscode !== false) {
+                $jsc .= "{$jscode}\n";
+                continue;
+            }
+            $err .= $fname . '; ';
+        }
+
+        if ($err != '') {
+            // error: missing files
+            $jscomp = "alert('The following JavaScript files could not be read:\\n{$err}');";
+        }
+        if ($jsc != '') {
+            $jscomp .= $jsc;
+        }
+        // output content
+        header('Content-type: text/javascript');
+        echo $jscomp;
+    }
+
+    private static function doRebuild($afiles, $tmpdir, $mtime, $lib_dir)
+    {
+        $hfname = hash('md5', implode($afiles), false) . '.js';
+        $cache_file = "{$tmpdir}/{$hfname}";
+        if (!is_readable($cache_file) || $mtime > filemtime($cache_file)) {
+            $phpcc = new \tureki\PhpCc([
                 'java_file'    => '/usr/bin/java -server -XX:+TieredCompilation',
                 'jar_file'     => "{$lib_dir}/compiler.jar",
                 'output_path'  => $tmpdir,
                 'optimization' => 'SIMPLE_OPTIMIZATIONS',
-                ]);
-                $c->add($afiles);
-                $ret = $c->exec($hfname);
-                if ($ret['status'] !== 0) {
-                    foreach ($ret['out'] as $out) {
-                        echo "console.error('" . str_replace("'", "\\'", $out) . "');" . PHP_EOL;
-                    }
+            ]);
+            $phpcc->add($afiles);
+            $ret = $phpcc->exec($hfname);
+            if ($ret['status'] !== 0) {
+                foreach ($ret['out'] as $out) {
+                    echo "console.error('" . str_replace("'", "\\'", $out) . "');" . PHP_EOL;
                 }
             }
-            if (is_readable($cache_file)) {
-                $cache_mtime = filemtime($cache_file);
-                $etag = md5_file($cache_file);
-                header("Last-Modified: ".gmdate("D, d M Y H:i:s", $cache_mtime)." GMT");
-                header("Etag: \"{$etag}\"");
-                if (@strtotime(@$_SERVER['HTTP_IF_MODIFIED_SINCE']) == $cache_mtime ||
-                @trim(@$_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
-                    header("HTTP/1.1 304 Not Modified");
-                } else {
-                    readfile($cache_file);
-                }
-            }
-            ob_end_flush();
-        } else {
-            $jsfiles = array();
-            foreach (explode('&', $files) as $part) {
-                $part = explode('=', $part);
-                if ($key = array_shift($part)) {
-                    if (empty($part)) {
-                        $jsfiles[] = $key;
-                    }
-                }
-            }
-
-            $js = '';        // code to compress
-            $err = '';       // error string
-            $jscomp = '';    // Final code
-            // fetch JavaScript files
-            for ($i = 0, $j = count($jsfiles); $i < $j; $i++) {
-                $fn = $jsfiles[$i] . '.js';
-                $jscode = @file_get_contents($fn);
-                if ($jscode !== false) {
-                    $js .= "{$jscode}\n";
-                } else {
-                    $err .= $fn . '; ';
-                }
-            }
-
-            if ($err != '') {
-                // error: missing files
-                $jscomp = "alert('The following JavaScript files could not be read:\\n{$err}');";
-            }
-            if ($js != '') {
-                $jscomp .= $js;
-            }
-            // output content
-            header('Content-type: text/javascript');
-            echo $jscomp;
         }
+        return $cache_file;
+    }
+
+    private static function closurejs($files, $tmpdir, $lib_dir)
+    {
+        $tmpdir = is_writable($tmpdir) ? $tmpdir : '/tmp/';
+        $afiles = [];
+        $mtime  = 0;
+        foreach (explode('&', $files) as $key) {
+            if (!strpos($key, '=')) {
+                $kfn = "{$key}.js";
+                $afiles[] = $kfn;
+                $cfmtime = filemtime($kfn);
+                if ($cfmtime > $mtime) {
+                    $mtime = $cfmtime;
+                }
+            }
+        }
+        header('Content-Type: text/javascript');
+        ob_start();
+        $cache_file = self::doRebuild($afiles, $tmpdir, $mtime, $lib_dir);
+        if (is_readable($cache_file)) {
+            $cache_mtime = filemtime($cache_file);
+            $etag = md5_file($cache_file);
+            header("Last-Modified: ".gmdate("D, d M Y H:i:s", $cache_mtime)." GMT");
+            header("Etag: \"{$etag}\"");
+            if (@strtotime(@$_SERVER['HTTP_IF_MODIFIED_SINCE']) == $cache_mtime ||
+            @trim(@$_SERVER['HTTP_IF_NONE_MATCH']) == $etag) {
+                header("HTTP/1.1 304 Not Modified");
+                return;
+            }
+            readfile($cache_file);
+        }
+        ob_end_flush();
     }
 }
