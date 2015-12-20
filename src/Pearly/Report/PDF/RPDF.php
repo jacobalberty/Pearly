9c86e2b8bbbc724e3892790c67a7b4c7e54a58ea
Jacob Alberty <jacob.alberty@vetmanage.com>
2015-12-20 15:31:28 -0600

    Add writeHTML to RPDF
    Based on FPDF licensed code at:
    http://www.fpdf.org/en/script/script42.php

--- src/Pearly/Report/PDF/RPDF.php
+++ src/Pearly/Report/PDF/RPDF.php
@@ -285,7 +285,7 @@ class RPDF extends \fpdi\tfpdf
         return $nl;
     }
 
-    function LineGraph($w, $h, $data, $options='', $colors=null, $maxVal=0, $nbDiv=4, $iopt = [])
+    public function lineGraph($w, $h, $data, $options = '', $colors = null, $maxVal = 0, $nbDiv = 4, $iopt = [])
     {
         /*******************************************
         Explain the variables:
@@ -315,7 +315,7 @@ class RPDF extends \fpdi\tfpdf
         ];
         $iopt = array_replace_recursive($ioptdef, $iopt);
 
-        $this->SetDrawColor(0,0,0);
+        $this->SetDrawColor(0, 0, 0);
         $this->SetLineWidth(0.2);
         $keys = array_keys($data);
         $ordinateWidth = 10;
@@ -339,75 +339,79 @@ class RPDF extends \fpdi\tfpdf
         $keyX = $valX+(2*$margin)+$graphW;
         $keyY = $valY+$margin+.5*($h-(2*$margin))-.5*($keyH);
         //draw graph frame border
-        if(strstr($options,'gB')){
-            $this->Rect($valX,$valY,$w,$h);
+        if (strstr($options, 'gB')) {
+            $this->Rect($valX, $valY, $w, $h);
         }
         //draw graph diagram border
-        if(strstr($options,'dB')){
-            $this->Rect($valX+$margin,$valY+$margin,$graphW,$graphH);
+        if (strstr($options, 'dB')) {
+            $this->Rect($valX+$margin, $valY+$margin, $graphW, $graphH);
         }
         //draw key legend border
-        if(strstr($options,'kB')){
-            $this->Rect($keyX,$keyY,$keyW,$keyH);
+        if (strstr($options, 'kB')) {
+            $this->Rect($keyX, $keyY, $keyW, $keyH);
         }
         //draw graph value box
-        if(strstr($options,'vB')){
-            $this->Rect($graphValX,$graphValY,$graphValW,$graphValH);
+        if (strstr($options, 'vB')) {
+            $this->Rect($graphValX, $graphValY, $graphValW, $graphValH);
         }
         //define colors
-        if($colors===null){
+        if ($colors===null) {
             $safeColors = array(0,51,102,153,204,225);
-            for($i=0;$i<count($data);$i++){
-                $colors[$keys[$i]] = array($safeColors[array_rand($safeColors)],$safeColors[array_rand($safeColors)],$safeColors[array_rand($safeColors)]);
+            for ($i=0; $i<count($data); $i++) {
+                $colors[$keys[$i]] = [
+                    $safeColors[array_rand($safeColors)],
+                    $safeColors[array_rand($safeColors)],
+                    $safeColors[array_rand($safeColors)]
+                ];
             }
         }
         //form an array with all data values from the multi-demensional $data array
         $ValArray = array();
-        foreach($data as $key => $value){
-            foreach($data[$key] as $val){
-                $ValArray[]=$val;                    
+        foreach ($data as $key => $value) {
+            foreach ($data[$key] as $val) {
+                $ValArray[]=$val;
             }
         }
         //define max value
-        if($maxVal<ceil(max($ValArray))){
+        if ($maxVal<ceil(max($ValArray))) {
             $maxVal = ceil(max($ValArray));
         }
         //draw horizontal lines
         $vertDivH = $graphH/$nbDiv;
-        if(strstr($options,'H')){
-            for($i=0;$i<=$nbDiv;$i++){
-                if($i<$nbDiv){
-                    $this->Line($graphX,$graphY+$i*$vertDivH,$graphX+$graphW,$graphY+$i*$vertDivH);
-                } else{
-                    $this->Line($graphX,$graphY+$graphH,$graphX+$graphW,$graphY+$graphH);
+        if (strstr($options, 'H')) {
+            for ($i=0; $i<=$nbDiv; $i++) {
+                if ($i<$nbDiv) {
+                    $this->Line($graphX, $graphY+$i*$vertDivH, $graphX+$graphW, $graphY+$i*$vertDivH);
+                } else {
+                    $this->Line($graphX, $graphY+$graphH, $graphX+$graphW, $graphY+$graphH);
                 }
             }
         }
         //draw vertical lines
         $horiDivW = floor($graphW/(count($data[$keys[0]])-1));
-        if(strstr($options,'V')){
-            for($i=0;$i<=(count($data[$keys[0]])-1);$i++){
-                if($i<(count($data[$keys[0]])-1)){
-                    $this->Line($graphX+$i*$horiDivW,$graphY,$graphX+$i*$horiDivW,$graphY+$graphH);
+        if (strstr($options, 'V')) {
+            for ($i=0; $i<=(count($data[$keys[0]])-1); $i++) {
+                if ($i<(count($data[$keys[0]])-1)) {
+                    $this->Line($graphX+$i*$horiDivW, $graphY, $graphX+$i*$horiDivW, $graphY+$graphH);
                 } else {
-                    $this->Line($graphX+$graphW,$graphY,$graphX+$graphW,$graphY+$graphH);
+                    $this->Line($graphX+$graphW, $graphY, $graphX+$graphW, $graphY+$graphH);
                 }
             }
         }
         //draw graph lines
-        foreach($data as $key => $value){
-            $this->setDrawColor($colors[$key][0],$colors[$key][1],$colors[$key][2]);
+        foreach ($data as $key => $value) {
+            $this->setDrawColor($colors[$key][0], $colors[$key][1], $colors[$key][2]);
             $this->SetLineWidth(0.8);
             $valueKeys = array_keys($value);
-            for($i=0;$i<count($value);$i++){
-                if($i==count($value)-2){
+            for ($i=0; $i<count($value); $i++) {
+                if ($i==count($value)-2) {
                     $this->Line(
                         $graphX+($i*$horiDivW),
                         $graphY+$graphH-($value[$valueKeys[$i]]/$maxVal*$graphH),
                         $graphX+$graphW,
                         $graphY+$graphH-($value[$valueKeys[$i+1]]/$maxVal*$graphH)
                     );
-                } else if($i<(count($value)-1)) {
+                } elseif ($i<(count($value)-1)) {
                     $this->Line(
                         $graphX+($i*$horiDivW),
                         $graphY+$graphH-($value[$valueKeys[$i]]/$maxVal*$graphH),
@@ -418,31 +422,331 @@ class RPDF extends \fpdi\tfpdf
             }
             //Set the Key (legend)
             $this->SetFont($iopt['keyf']['family'], $iopt['keyf']['style'], $iopt['keyf']['size']);
-            if(!isset($n))$n=0;
-            $this->Line($keyX+1,$keyY+$lineh/2+$n*$lineh,$keyX+8,$keyY+$lineh/2+$n*$lineh);
-            $this->SetXY($keyX+8,$keyY+$n*$lineh);
-            $this->Cell($keyW,$lineh,$key,0,1,'L');
+            if (!isset($n)) {
+                $n=0;
+            }
+            $this->Line($keyX+1, $keyY+$lineh/2+$n*$lineh, $keyX+8, $keyY+$lineh/2+$n*$lineh);
+            $this->SetXY($keyX+8, $keyY+$n*$lineh);
+            $this->Cell($keyW, $lineh, $key, 0, 1, 'L');
             $n++;
         }
         //print the abscissa values
-        foreach($valueKeys as $key => $value){
-            if($key==0){
-                $this->SetXY($graphValX,$graphValY);
-                $this->Cell(30,$lineh,$value,0,0,'L');
-            } else if($key==count($valueKeys)-1){
-                $this->SetXY($graphValX+$graphValW-30,$graphValY);
-                $this->Cell(30,$lineh,$value,0,0,'R');
-            } else if($key % $iopt['abs']['mod'] == 0){
-                $this->SetXY($graphValX+$key*$horiDivW-15,$graphValY);
-                $this->Cell(30,$lineh,$value,0,0,'C');
+        foreach ($valueKeys as $key => $value) {
+            if ($key==0) {
+                $this->SetXY($graphValX, $graphValY);
+                $this->Cell(30, $lineh, $value, 0, 0, 'L');
+            } elseif ($key==count($valueKeys)-1) {
+                $this->SetXY($graphValX+$graphValW-30, $graphValY);
+                $this->Cell(30, $lineh, $value, 0, 0, 'R');
+            } elseif ($key % $iopt['abs']['mod'] == 0) {
+                $this->SetXY($graphValX+$key*$horiDivW-15, $graphValY);
+                $this->Cell(30, $lineh, $value, 0, 0, 'C');
             }
         }
         //print the ordinate values
-        for($i=0;$i<=$nbDiv;$i++){
-            $this->SetXY($graphValX-10,$graphY+($nbDiv-$i)*$vertDivH-3);
-            $this->Cell(8,6,sprintf('%.1f',$maxVal/$nbDiv*$i),0,0,'R');
+        for ($i=0; $i<=$nbDiv; $i++) {
+            $this->SetXY($graphValX-10, $graphY+($nbDiv-$i)*$vertDivH-3);
+            $this->Cell(8, 6, sprintf('%.1f', $maxVal/$nbDiv*$i), 0, 0, 'R');
         }
-        $this->SetDrawColor(0,0,0);
+        $this->SetDrawColor(0, 0, 0);
         $this->SetLineWidth(0.2);
+    }
+
+    private $B=0;
+    private $I=0;
+    private $U=0;
+    private $HREF='';
+    private $fontList=array("Times", "Courier");
+    private $PRE=false;
+    private $issetfont=false;
+    private $issetcolor=false;
+    private $origFontSize;
+
+    private function hex2dec($color = "#000000")
+    {
+        $tbl_color = array();
+        $tbl_color['R']=hexdec(substr($color, 1, 2));
+        $tbl_color['G']=hexdec(substr($color, 3, 2));
+        $tbl_color['B']=hexdec(substr($color, 5, 2));
+        return $tbl_color;
+    }
+
+    private function px2mm($px)
+    {
+        return $px*25.4/72;
+    }
+
+    private function txtentities($html)
+    {
+        $trans = get_html_translation_table(HTML_ENTITIES);
+        $trans = array_flip($trans);
+        return strtr($html, $trans);
+    }
+
+    public function writeHTML($height, $html, $ln = true)
+    {
+        $html=strip_tags(
+            $html,
+            "<a><img><p><br><font><tr><blockquote><h1><h2><h3><h4><pre><red><blue><ul><li><hr><b><i><u><strong><em>"
+        );
+        $html=str_replace("\n", ' ', $html); //replace carriage returns by spaces
+
+        $html = str_replace('&trade;', '™', $html);
+        $html = str_replace('&copy;', '©', $html);
+        $html = str_replace('&euro;', '€', $html);
+
+        $a=preg_split('/<(.*)>/U', $html, -1, PREG_SPLIT_DELIM_CAPTURE);
+        $skip=false;
+        foreach ($a as $i => $e) {
+            if (!$skip) {
+                if ($this->HREF) {
+                    $e=str_replace("\n", "", str_replace("\r", "", $e));
+                }
+                if ($i%2==0) {
+                // new line
+                    if ($this->PRE) {
+                        $e=str_replace("\r", "\n", $e);
+                    } else {
+                        $e=str_replace("\r", "", $e);
+                    }
+                    //Text
+                    if ($this->HREF) {
+                        $this->putLink($this->HREF, $e);
+                        $skip=true;
+                    } else {
+                        $this->Write($height, stripslashes($this->txtentities($e)));
+                    }
+                } else {
+                    //Tag
+                    if (substr(trim($e), 0, 1)=='/') {
+                        $this->closeTag(strtoupper(substr($e, strpos($e, '/')+1)));
+                    } else {
+                        //Extract attributes
+                        $a2=explode(' ', $e);
+                        $tag=strtoupper(array_shift($a2));
+                        $attr=array();
+                        foreach ($a2 as $v) {
+                            if (preg_match('/([^=]*)=["\']?([^"\']*)/', $v, $a3)) {
+                                $attr[strtoupper($a3[1])]=$a3[2];
+                            }
+                        }
+                        $this->openTag($tag, $attr);
+                    }
+                }
+            } else {
+                $this->HREF='';
+                $skip=false;
+            }
+        }
+        if ($ln) {
+            $this->Ln();
+        }
+    }
+
+    private function openTag($tag, $attr, $height = 5)
+    {
+        $this->origFontSize = $this->FontSizePt;
+        //Opening tag
+        switch ($tag) {
+            case 'STRONG':
+            case 'B':
+                $this->setStyle('B', true);
+                break;
+            case 'H1':
+                $this->Ln(5);
+                $this->setTextColor(150, 0, 0);
+                $this->setFontSize(22);
+                break;
+            case 'H2':
+                $this->Ln(5);
+                $this->setFontSize(18);
+                $this->setStyle('U', true);
+                break;
+            case 'H3':
+                $this->Ln(5);
+                $this->setFontSize(16);
+                $this->setStyle('U', true);
+                break;
+            case 'H4':
+                $this->Ln(5);
+                $this->setTextColor(102, 0, 0);
+                $this->setFontSize(14);
+                $this->setStyle('B', true);
+                break;
+            case 'PRE':
+                $this->setFont('Courier', '', 11);
+                $this->setFontSize(11);
+                $this->setStyle('B', false);
+                $this->setStyle('I', false);
+                $this->PRE=true;
+                break;
+            case 'RED':
+                $this->SetTextColor(255, 0, 0);
+                break;
+            case 'BLOCKQUOTE':
+                $this->resetableSetTextColor(100, 0, 45);
+                $this->Ln(3);
+                break;
+            case 'BLUE':
+                $this->setTextColor(0, 0, 255);
+                break;
+            case 'I':
+            case 'EM':
+                $this->setStyle('I', true);
+                break;
+            case 'U':
+                $this->setStyle('U', true);
+                break;
+            case 'A':
+                $this->HREF=$attr['HREF'];
+                break;
+            case 'IMG':
+                if (isset($attr['SRC']) && (isset($attr['WIDTH']) || isset($attr['HEIGHT']))) {
+                    if (!isset($attr['WIDTH'])) {
+                        $attr['WIDTH'] = 0;
+                    }
+                    if (!isset($attr['HEIGHT'])) {
+                        $attr['HEIGHT'] = 0;
+                    }
+                    $this->Image(
+                        $attr['SRC'],
+                        $this->GetX(),
+                        $this->GetY(),
+                        $this->px2mm($attr['WIDTH']),
+                        $this->px2mm($attr['HEIGHT'])
+                    );
+                    $this->Ln(3);
+                }
+                break;
+            case 'LI':
+                $this->Ln(2);
+                $this->SetTextColor(190, 0, 0);
+                $this->Write($height, '     » ');
+                $this->resetableSetTextColor(-1);
+                break;
+            case 'BR':
+                $this->Ln();
+                break;
+            case 'P':
+                $this->Ln($height*1.5);
+                break;
+            case 'HR':
+            case 'TR':
+                $this->Ln(2);
+                $this->Line($this->GetX(), $this->GetY(), $this->GetX()+187, $this->GetY());
+                $this->Ln(3);
+                break;
+            case 'FONT':
+                if (isset($attr['COLOR']) && $attr['COLOR']!='') {
+                    $coul=$this->hex2dec($attr['COLOR']);
+                    $this->resetableSetTextColor($coul['R'], $coul['G'], $coul['B']);
+                    $this->issetcolor=true;
+                }
+                if (isset($attr['FACE']) && in_array(strtolower($attr['FACE']), $this->fontlist)) {
+                    $this->SetFont(strtolower($attr['FACE']));
+                    $this->issetfont=true;
+                }
+                break;
+        }
+    }
+
+    private function closeTag($tag)
+    {
+        switch ($tag) {
+            case 'H1':
+            case 'H2':
+            case 'H3':
+            case 'H4':
+                $this->Ln(6);
+                $this->setFontSize($this->origFontSize);
+                $this->setStyle('U', false);
+                $this->setStyle('B', false);
+                $this->resetableSetTextColor(-1);
+                break;
+            case 'PRE':
+                $this->SetFont('', '', 12);
+                $this->SetFontSize($this->orgFont);
+                $this->PRE=false;
+                break;
+            case 'RED':
+            case 'BLUE':
+                $this->resetableSetTextColor(-1);
+                break;
+            case 'BLOCKQUOTE':
+                $this->resetableSetTextColor(0, 0, 0);
+                $this->Ln(3);
+                break;
+            case 'STRONG':
+                $this->setStyle('B', false);
+                break;
+            case 'EM':
+                $this->setStyle('I', false);
+                break;
+            case 'B':
+            case 'I':
+            case 'U':
+                $this->setStyle($tag, false);
+                break;
+            case 'A':
+                $this->HREF='';
+                break;
+            case 'FONT':
+                if ($this->issetcolor==true) {
+                    $this->setTextColor(0, 0, 0);
+                }
+                if ($this->issetfont) {
+                    $this->setFont('Times', '', 12);
+                    $this->issetfont=false;
+                }
+                break;
+        }
+    }
+
+    private function setStyle($tag, $enable)
+    {
+        $this->$tag+=($enable ? 1 : -1);
+        $style='';
+        foreach (array('B','I','U') as $s) {
+            if ($this->$s>0) {
+                $style.=$s;
+            }
+        }
+        $this->setFont('', $style);
+    }
+
+    private function putLink($URL, $txt, $height = 5)
+    {
+        //Put a hyperlink
+        $this->SetTextColor(0, 0, 255);
+        $this->setStyle('U', true);
+        $this->Write($height, $txt, $URL);
+        $this->setStyle('U', false);
+        $this->resetableSetTextColor(-1);
+    }
+
+    private function resetableSetTextColor($r, $g = 0, $b = 0)
+    {
+        static $_r=0, $_g=0, $_b=0;
+
+        if ($r==-1) {
+            $this->SetTextColor($_r, $_g, $_b);
+            return;
+        }
+        $this->SetTextColor($r, $g, $b);
+        $_r=$r;
+        $_g=$g;
+        $_b=$b;
+    }
+
+    public function writeHTMLCell($height, $html)
+    {
+        $olMargin = $this->lMargin;
+        $x = $this->getX();
+        $this->setLeftMargin($x);
+        // Hack to avoid the first paragraph from parsedown
+        $html = substr($html, 0, 3) === '<p>' ? substr($html, 3) : $html;
+        $this->writeHTML($height, $html);
+        $this->setLeftMargin($olMargin);
+        $this->setX($olMargin);
     }
 }
